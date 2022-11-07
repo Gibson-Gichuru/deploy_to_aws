abcacb0c3604d25ca8bb295c176706dba81a8278
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-11-07 11:29:11 +0300

    Test Runner Setup
    Tests can now be run with flask test command

---
+++ manage.py
@@ -0,0 +1,23 @@
+import sys
+
+import os
+
+from app import create_app()
+
+app = create_app(os.environ.get("FLASK_ENV") or "default")
+
+
+@app.cli.command()
+def test():
+
+    import unittest
+
+    # running unittests
+
+    tests  = unittest.TestLoader().discover("tests")
+
+    results = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if not results.wasSuccessful():
+
+        sys.exit()
