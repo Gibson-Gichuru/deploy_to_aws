2737dad2c6cc8d60dd98056500845ff5dd5f3abb
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-11-07 12:02:06 +0300

    Main route testing

---
+++ tests/test_app_routes.py
@@ -0,0 +1,20 @@
+from tests import TestBaseConfig
+
+
+class TestAppRoutes(TestBaseConfig):
+
+    def setUp(self):
+
+        super().setUp()
+
+        self.client = self.app.test_client()
+
+
+    def test_home_route(self):
+
+        response = self.client.get("/").get_json()
+
+        self.assertDictEqual(response, {
+            "content":"sample Application",
+            "purpose":"Test Driven development",
+        })
