54cfbab576cb8e73d8f25c0ce1fc7f023975c832
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-11-20 08:40:58 +0300

    applicaton deployment script setup

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,72 @@
 #!/bin/bash
 
-echo "deployment script"
+set -e
 
+trap 'current_command=$BASH_COMMAND' DEBUG
+
+trap 'echo "\"${current_command}\" command filed with exit code $?."' EXIT
+
+
+function run_database_migrations(){
+
+    echo "Running database migrations"
+}
+
+function deploy_updates(){
+
+    PWD=$(pwd)
+
+    if [ -f "/etc/systemd/system/api.service" ]; then
+
+        echo "Api Service file Found"
+
+        # Api status check 
+
+        API_STATUS=`sudo systemctl status api`
+
+
+        if [ "$API_STATUS" == "active" ]; then 
+
+            echo "Stoping api service"
+
+            sudo systemctl stop api
+
+        fi
+
+        # install any dependencies
+
+        $PWD/env/bin/pip install -r $PWD/requirements.txt
+
+        # perform and database migrations
+
+        run_database_migrations
+
+        # reload-or-start the api service
+
+        systemctl reload-or-restart api 
+
+
+    else
+
+        echo "Api Service file not found"
+
+        sudo cp $PWD/api.service "/etc/systemd/system/"
+
+        # install any dependencies
+
+        $PWD/env/bin/pip install -r $PWD/requirements.txt
+
+        # perform and database migrations
+
+        run_database_migrations
+
+        # reload-or-start the api service
+
+        systemctl reload-or-restart api 
+
+
+
+    fi
+}
+
+deploy_updates
