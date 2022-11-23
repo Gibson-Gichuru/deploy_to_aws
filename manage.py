63cefe11a137b499e0d19ba11a57c72f70d76619
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-11-23 11:01:12 +0300

    Serving application with gunicorn

--- manage.py
+++ manage.py
@@ -4,10 +4,10 @@ import os
 
 from app import create_app
 
-app = create_app(os.environ.get("FLASK_ENV") or "default")
+application = create_app(os.environ.get("FLASK_ENV") or "default")
 
 
-@app.cli.command()
+@application.cli.command()
 def test():
 
     import unittest
